27fcd1f504df8c7dcb39cb168876f8138b071019
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-11 04:24:44 +0900

    base64

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
+use base64::{encode};
 
 fn main() {
     let key = "this_is_key".as_bytes();
     let mut hasher = Sha1::new();
     hasher.input(key);
     let sha1_string = hasher.result_str();
-    println!("{}", sha1_string);
+    let base64_sha1_string = encode(sha1_string);
+    println!("{}", base64_sha1_string);
 }
